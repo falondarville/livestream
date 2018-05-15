d1859971739d8824357691fcd72dfae3273faec3
Falon Darville <falondarville@gmail.com>
2018-05-15 15:03:01 -0700

    ajax for videoID and live chat

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const nodemon = require('nodemon');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views' + '/index.html'));
+});
+
+app.use('/', express.static('public'));
+
+app.listen(PORT, function() {
+  console.log(`The server is up and running on port: ${PORT} .`);
+});
