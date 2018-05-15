32bb048055bae539c3f6163dd274036c0a260e51
Falon Darville <falondarville@gmail.com>
2018-05-15 16:44:16 -0700

    increase height of chat window

--- public/script.js
+++ public/script.js
@@ -6,10 +6,10 @@ function onSignIn(googleUser) {
 		url:'https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&key=AIzaSyDmxmFz0AMMLMxzApA2x0no3r3VZlJdeAU&limit=5'
 	}).done(function(data){
 		$('#data').empty();
-		var videoID =  data.items[1].id.videoId;
+		var videoID =  data.items[4].id.videoId;
 		var videoIDurl = 'https://www.youtube.com/embed/' + videoID;
 		$('#data').append('<iframe width="560" height="315" src=' + videoIDurl + ' frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
-		$('#data').append('<iframe src="https://www.youtube.com/live_chat?v=' + videoID + '&embed_domain=' + window.location.hostname + '"></iframe>');
+		$('#data').append('<iframe height="315" src="https://www.youtube.com/live_chat?v=' + videoID + '&embed_domain=' + window.location.hostname + '"></iframe>');
 
 		// $.ajax({
 		// 	url: 'https://www.googleapis.com/youtube/v3/videos/?id=' + videoID + '&part=liveStreamingDetails,status,contentDetails,snippet,id&key=AIzaSyDmxmFz0AMMLMxzApA2x0no3r3VZlJdeAU'
