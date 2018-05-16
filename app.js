2de18fcd9c73a4234307a0c529d27ef9245f7f05
Falon Darville <falondarville@gmail.com>
2018-05-15 17:59:10 -0700

    video and chat

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
 const nodemon = require('nodemon');
+const dotenv = require('dotenv').config({
+	path: './.env'
+});
+const exphbs = require('express-handlebars');
+const axios = require('axios');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views' + '/index.html'));
+    axios.get('https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&key=' + process.env.GOOGLE_API + '&limit=5')
+    .then(function(response){
+		var videoID =  response.data.items[4].id.videoId;
+		var videoIDurl = 'https://www.youtube.com/embed/' + videoID;
+		res.render('index', {videoID, videoIDurl, hostname: req.hostname});
+    }).catch(function(error){
+    	console.log(error.response.data)
+    })
 });
 
 app.use('/', express.static('public'));
--- public/script.js
+++ public/script.js
@@ -2,22 +2,5 @@ function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
 
 	$('.g-signin2').attr('style', 'display: none');
-	$.ajax({
-		url:'https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&key=AIzaSyDmxmFz0AMMLMxzApA2x0no3r3VZlJdeAU&limit=5'
-	}).done(function(data){
-		$('#data').empty();
-		var videoID =  data.items[4].id.videoId;
-		var videoIDurl = 'https://www.youtube.com/embed/' + videoID;
-		$('#data').append('<iframe width="560" height="315" src=' + videoIDurl + ' frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
-		$('#data').append('<iframe height="315" src="https://www.youtube.com/live_chat?v=' + videoID + '&embed_domain=' + window.location.hostname + '"></iframe>');
-
-		// $.ajax({
-		// 	url: 'https://www.googleapis.com/youtube/v3/videos/?id=' + videoID + '&part=liveStreamingDetails,status,contentDetails,snippet,id&key=AIzaSyDmxmFz0AMMLMxzApA2x0no3r3VZlJdeAU'
-		// }).done(function(response){
-		// 	var liveChatID = response.items[0].liveStreamingDetails.activeLiveChatId
-		// 	console.log(liveChatID)
-		// $('#data').append('<iframe allowfullscreen="" frameborder="0" height="270" src="https://www.youtube.com/live_chat?v=' + liveChatID + '&width="480"></iframe>');
-		// })
-	})
 }
 
